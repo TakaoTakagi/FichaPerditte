d8048bb8570c0db1c24ce1fe16dcf69c8b0ca6f5
TakaoTakagi <deosanches@hotmail.com>
2025-06-26 22:33:02 -0300

    Teste com AppTeste no lugar do App original

---
+++ src/AppTeste.js
@@ -0,0 +1,8 @@
+// src/AppTeste.js
+export default function AppTeste() {
+  return (
+    <div style={{ padding: '2rem', fontSize: '1.5rem', color: 'black' }}>
+      Teste simples: está funcionando!
+    </div>
+  );
+}
